ca61ea92333042de6f2b43361a3b1a60a50cf60d
Victor <viclee02@gmail.com>
2017-07-10 20:21:18 -1000

    fizzBuzz exercise

---
+++ app.js
@@ -0,0 +1,6 @@
+/*Create a for loop that will iterate through 100 numbers starting from 1 and do the following:
+
+if the number is a multiple of 3, it will console.log "fizz",
+if the number is a multiple of 5, it will console.log "buzz",  
+if the number is a multiple of 3 and 5, it will console.log "fizzBuzz"
+*/
