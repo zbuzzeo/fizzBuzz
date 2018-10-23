98a0a407ac739deff1fed0c611bb5b41b3ec570b
unknown <zeke.buzzeo@gmail.com>
2018-10-22 18:26:37 -1000

    fizzBuzz complete!

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ if the number is a multiple of 3, it will console.log "fizz",
 if the number is a multiple of 5, it will console.log "buzz",  
 if the number is a multiple of 3 and 5, it will console.log "fizzBuzz"
 */
+
+for (var i = 1; i <= 100; i++) {
+      //    The statement with the && operator needs to be the first statement because it would be skipped over if it were checked last.
+      if (i % 3 === 0 && i % 5 === 0) {
+            console.log(`FIZZbuzz: ${i}`);
+
+      } else if (i % 3 === 0) {
+            console.log(`FIZZ: ${i}`);
+
+      } else if (i % 5 === 0) {
+            console.log(`buzz: ${i}`);
+      }
+
+}
